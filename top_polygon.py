5327b8d864b13ba4c4e54b6bfc0b27c600c44d7c
ZenkerArt <95053537+ZenkerArt@users.noreply.github.com>
2022-08-10 22:13:50 +0300

    Add files via upload

---
+++ top_polygon.py
@@ -0,0 +1,117 @@
+import random
+
+import bpy
+
+from bpy.types import Mesh
+
+bl_info = {
+    'name': 'Top Polygon',
+    'blender': (2, 80, 0),
+    'category': 'Object',
+    'author': 'Zenker'
+}
+
+
+def update(self, context):
+    scene = context.scene
+
+    b = context.scene.top_object[context.scene.top_object_active]
+    bpy.ops.object.select_all(action='DESELECT')
+
+    ob = bpy.data.objects[b.name]
+    bpy.context.view_layer.objects.active = ob
+    ob.select_set(True)
+
+
+class TopObject(bpy.types.PropertyGroup):
+    name: bpy.props.StringProperty()
+    polycount: bpy.props.IntProperty(default=0)
+
+
+class TopSettings(bpy.types.PropertyGroup):
+    modify: bpy.props.BoolProperty(default=True)
+
+
+class BasePanel:
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'TOP'
+    bl_context = 'objectmode'
+
+
+class TOP_UL_top_props(bpy.types.UIList):
+
+    def filter_items(self, context, data, propname):
+        helper_funcs = bpy.types.UI_UL_list
+
+        o = getattr(data, propname).values()
+
+        ordered = [(index, item.polycount) for index, item in enumerate(o)]
+        ordered = helper_funcs.sort_items_helper(ordered, lambda e: e[1], True)
+
+        return [], ordered
+
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            row = layout.row()
+            row.label(text=item.name)
+            row.label(text=str(item.polycount))
+
+
+class TOP_OT_top(bpy.types.Operator):
+    bl_idname: str = 'top.calc_top'
+    bl_label: str = 'Calc top'
+
+    def execute(self, context: bpy.types.Context):
+        scene = context.scene
+        scene.top_object.clear()
+        dg = context.evaluated_depsgraph_get()
+
+        for i in bpy.data.objects:
+            if not isinstance(i.data, Mesh):
+                continue
+
+            if scene.top_settings.modify:
+                i = i.evaluated_get(dg)
+
+            item = scene.top_object.add()
+            item.name = i.name
+            item.polycount = len(i.data.polygons)
+        return {'FINISHED'}
+
+
+class TOP_PT_object_list(BasePanel, bpy.types.Panel):
+    bl_label = 'Топ'
+
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+        layout.prop(scene.top_settings, 'modify', text='Учесть Модификаторы')
+        layout.template_list('TOP_UL_top_props', '',
+                             scene, 'top_object', scene, 'top_object_active')
+
+        layout.operator(TOP_OT_top.bl_idname, text='Посчитать')
+
+
+reg, unreg = bpy.utils.register_classes_factory((
+    TOP_OT_top,
+    TOP_PT_object_list,
+    TOP_UL_top_props,
+    TopObject,
+    TopSettings
+))
+
+
+def register():
+    reg()
+    bpy.types.Scene.top_settings = bpy.props.PointerProperty(type=TopSettings)
+    bpy.types.Scene.top_object = bpy.props.CollectionProperty(type=TopObject)
+    bpy.types.Scene.top_object_active = bpy.props.IntProperty(update=update)
+
+
+def unregister():
+    unreg()
+
+
+if __name__ == '__main__':
+    register()
